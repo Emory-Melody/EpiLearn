0e1490e32303aa1e0d183e9d8bbb39063b4e0c43
Yunxiao_Li <141427881+Momo23569@users.noreply.github.com>
2024-05-22 23:44:04 -0400

    Update XGB.py

--- epilearn/models/Temporal/XGB.py
+++ epilearn/models/Temporal/XGB.py
@@ -4,14 +4,40 @@ import torch.nn.init as init
 import torch
 
 class XGBModel:
-    def __init__(self, num_features, num_timesteps_input, num_timesteps_output, nhid=256, dropout=0.5, use_norm=False,
+    """
+        Extreme Gradient Boosting (XGBoost) Model
+        
+        Parameters
+        ----------
+        num_features : int
+            Number of features in each timestep of the input data.
+        num_timesteps_input : int
+            Number of timesteps considered for each input sample.
+        num_timesteps_output : int
+            Number of output timesteps to predict.
+        n_estimators : int, optional
+            Number of gradient boosted trees. Equivalent to the number of boosting rounds. Default: 40.
+        learning_rate : float, optional
+            Step size shrinkage used in update to prevents overfitting. Default: 0.1.
+        max_depth : int, optional
+            Maximum depth of a tree. Increasing this value will make the model more complex and more likely to overfit. Default: 5.
+        reg_lambda : float, optional
+            L2 regularization term on weights. Increasing this value will make model more conservative. Default: 1.0.
+        reg_alpha : float, optional
+            L1 regularization term on weights. Increasing this value will make model more conservative. Default: 0.1.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor of shape (batch_size, num_timesteps_output) representing the predicted values for the future timesteps.
+            Each element corresponds to a predicted value for a future timestep.
+        
+    """
+    def __init__(self, num_features, num_timesteps_input, num_timesteps_output,
                  n_estimators=40, learning_rate=0.1, max_depth=5, reg_lambda=1.0, reg_alpha=0.1):
         self.num_features = num_features
         self.num_timesteps_input = num_timesteps_input
         self.num_timesteps_output = num_timesteps_output
-        self.nhid = nhid
-        self.dropout = dropout
-        self.use_norm = use_norm
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.max_depth = max_depth
@@ -20,8 +46,8 @@ class XGBModel:
         self.model = None
 
     def get_features(self, X):
-        # X is a NumPy array; reshape it according to num_features and num_timesteps_input
-        return X.reshape(-1, self.num_features * self.num_timesteps_input)
+        features = X.reshape(-1, self.num_features * self.num_timesteps_input)
+        return features
 
     def fit(self, train_input, train_target, val_input=None, val_target=None, epochs=1000, batch_size=10, verbose=False, patience=100):
         train_input = self.get_features(train_input)
@@ -36,8 +62,8 @@ class XGBModel:
             n_estimators=self.n_estimators, 
             learning_rate=self.learning_rate, 
             max_depth=self.max_depth,
-            reg_lambda=self.reg_lambda,  # L2 regularization factor
-            reg_alpha=self.reg_alpha    # L1 regularization factor
+            reg_lambda=self.reg_lambda,  
+            reg_alpha=self.reg_alpha    
         )
 
         if val_input is not None and val_target is not None:
