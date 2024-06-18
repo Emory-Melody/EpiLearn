97477b95aa89553eeab8a4aa907a20f44bbe64ef
Yunxiao_Li <141427881+Momo23569@users.noreply.github.com>
2024-06-18 19:57:22 -0400

    Update utils.py

--- epilearn/utils/utils.py
+++ epilearn/utils/utils.py
@@ -32,6 +32,23 @@ def accuracy(output, labels):
 
 
 def normalize(X):
+    """
+    Normalizes the input tensor X to have zero mean and unit standard deviation. Handles 3D tensors.
+    
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input tensor to be normalized.
+
+    Returns
+    -------
+    torch.Tensor
+        The normalized tensor.
+    torch.Tensor
+        The means of the input tensor.
+    torch.Tensor
+        The standard deviations of the input tensor.
+    """
     if len(X.shape) == 3:
         means = torch.mean(X, axis=(0, 1))
         X = X - means.unsqueeze(0).unsqueeze(0)
@@ -44,6 +61,16 @@ def normalize(X):
 def normalize_adj(Adj):
     """
     Returns the degree normalized adjacency matrix.
+    
+    Parameters
+    ----------
+    Adj : torch.Tensor or np.array
+        The input adjacency matrix.
+
+    Returns
+    -------
+    torch.Tensor
+        The degree normalized adjacency matrix.
     """
     if Adj is None:
         return None
@@ -74,10 +101,36 @@ def normalize_adj(Adj):
     return torch.FloatTensor(A_wave)
 
 def diff(features):
+    """
+    Computes the discrete difference along the time dimension of the input tensor.
+    
+    Parameters
+    ----------
+    features : torch.Tensor
+        The input feature tensor.
+
+    Returns
+    -------
+    torch.Tensor
+        The tensor of differences along the time dimension.
+    """
     return torch.diff(features, dim=0)
 
 
 def Degree_Matrix(ST_matrix):
+    """
+    Computes the degree matrix for a given spatio-temporal adjacency matrix.
+    
+    Parameters
+    ----------
+    ST_matrix : torch.Tensor
+        The input spatio-temporal adjacency matrix.
+
+    Returns
+    -------
+    torch.Tensor
+        The degree matrix.
+    """
 
     row_sum = torch.sum(ST_matrix, 0)
 
@@ -94,11 +147,23 @@ The binary spatio-temporal adjacency matrix used in USTGCN
 """
 def Static_full(n, t, A):
     """
-    :param n: the dimension of the spatial adjacency matrix
-    :param t: the length of periods
-    :param A: the spatial adjacency matrix
-    :return: the full USTGCN spatio-temporal adjacency matrix
+    Constructs the full spatio-temporal adjacency matrix using the binary spatio-temporal adjacency matrix method.
+    
+    Parameters
+    ----------
+    n : int
+        The dimension of the spatial adjacency matrix.
+    t : int
+        The length of periods.
+    A : torch.Tensor
+        The spatial adjacency matrix.
+
+    Returns
+    -------
+    torch.Tensor
+        The full spatio-temporal adjacency matrix.
     """
+
     I_S = torch.diag_embed(torch.ones(n))
     I_T = torch.diag_embed(torch.ones(t))
 
@@ -116,15 +181,40 @@ Use kronecker product to construct the spatio-temporal adjacency matrix
 """
 def kronecker(A, B):
     """
-    :param A: the temporal adjacency matrix
-    :param B: the spatial adjacency matrix
-    :return: the adjacency matrix of one space-time neighboring block
+    Constructs the spatio-temporal adjacency matrix using the Kronecker product.
+    
+    Parameters
+    ----------
+    A : torch.Tensor
+        The temporal adjacency matrix.
+    B : torch.Tensor
+        The spatial adjacency matrix.
+
+    Returns
+    -------
+    torch.Tensor
+        The adjacency matrix of one space-time neighboring block.
     """
     AB = torch.einsum("ab,cd->acbd", A, B)
     AB = AB.contiguous().view(A.size(0)*B.size(0), A.size(1)*B.size(1))
     return AB
 
 def edge_to_adj(edge_index, num_nodes):
+    """
+    Converts edge index representation to adjacency matrix.
+    
+    Parameters
+    ----------
+    edge_index : torch.Tensor
+        The edge index tensor where each column represents an edge.
+    num_nodes : int
+        The number of nodes in the graph.
+
+    Returns
+    -------
+    torch.Tensor
+        The adjacency matrix.
+    """
     adj = torch.zeros(num_nodes, num_nodes)
     adj[edge_index[0], edge_index[1]] = 1
     adj[edge_index[1], edge_index[0]] = 1
@@ -148,7 +238,15 @@ def edge_to_adj(edge_index, num_nodes):
 
 class moving_avg(nn.Module):
     """
-    Moving average block to highlight the trend of time series
+    Moving average block to highlight the trend of time series. This module smooths the input time series data 
+    using a moving average filter, which helps in capturing the underlying trend by averaging over a specified window.
+
+    Parameters
+    ----------
+    kernel_size : int
+        The size of the moving average window.
+    stride : int
+        The stride of the moving average window.
     """
 
     def __init__(self, kernel_size, stride):
@@ -157,6 +255,20 @@ class moving_avg(nn.Module):
         self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
 
     def forward(self, x):
+        """
+        Applies the moving average to the input tensor 'x', padding the time series at both ends to ensure
+        that the moving average is computed correctly for the entire series.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input time-series data tensor, typically of shape (batch_size, time_steps, features).
+
+        Returns
+        -------
+        torch.Tensor
+            The tensor smoothed using the moving average, maintaining the original shape of the input.
+        """
         # padding on the both ends of time series
         front = x[:, 0:1, :].repeat(1, self.kernel_size - 1 - math.floor((self.kernel_size - 1) // 2), 1)
         end = x[:, -1:, :].repeat(1, math.floor((self.kernel_size - 1) // 2), 1)
@@ -168,14 +280,37 @@ class moving_avg(nn.Module):
 
 class series_decomp(nn.Module):
     """
-    Series decomposition block
-    """
+    A PyTorch module for series decomposition using a single moving average kernel. This module decomposes
+    a time series into its residual and moving average components, providing a straightforward approach to
+    trend extraction.
 
+    Parameters
+    ----------
+    kernel_size : int
+        The kernel size to be used for the moving average calculation. The kernel size defines the window
+        for the moving average.
+    """
     def __init__(self, kernel_size):
         super(series_decomp, self).__init__()
         self.moving_avg = moving_avg(kernel_size, stride=1)
 
     def forward(self, x):
+        """
+        Applies the series decomposition to the input tensor 'x'. It calculates the moving average using the specified kernel size
+        and computes the residual by subtracting the moving average from the original series.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input time-series data tensor, typically of shape (batch_size, time_steps).
+
+        Returns
+        -------
+        tuple(torch.Tensor, torch.Tensor)
+            A tuple containing:
+            - The residual tensor after subtracting the moving average from the input.
+            - The moving average tensor computed from the input.
+        """
         moving_mean = self.moving_avg(x)
         res = x - moving_mean
         return res, moving_mean
@@ -183,7 +318,16 @@ class series_decomp(nn.Module):
 
 class series_decomp_multi(nn.Module):
     """
-    Series decomposition block
+    A PyTorch module for series decomposition using multiple moving average kernels. This module decomposes
+    a time series into its residual and moving average components, leveraging multiple kernel sizes for enhanced
+    flexibility and accuracy in capturing trends.
+
+    Parameters
+    ----------
+    kernel_size : list of int
+        List of kernel sizes to be used for moving average calculations. Each kernel size defines the window
+        for the moving average.
+
     """
 
     def __init__(self, kernel_size):
@@ -192,6 +336,23 @@ class series_decomp_multi(nn.Module):
         self.layer = torch.nn.Linear(1, len(kernel_size))
 
     def forward(self, x):
+        """
+        Applies the series decomposition to the input tensor 'x'. It calculates multiple moving averages
+        using the initialized kernels, weights them using a linear layer followed by a softmax, and computes
+        the residual by subtracting the weighted moving average from the original series.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input time-series data tensor, typically of shape (batch_size, time_steps).
+
+        Returns
+        -------
+        tuple(torch.Tensor, torch.Tensor)
+            A tuple containing:
+            - The residual tensor after subtracting the weighted moving average from the input.
+            - The weighted moving average tensor computed from the input.
+        """
         moving_mean = []
         for func in self.moving_avg:
             moving_avg = func(x)
@@ -205,7 +366,24 @@ class series_decomp_multi(nn.Module):
 
 
 class FourierLayer(nn.Module):
+    """
+    A PyTorch module that applies Fourier Transform techniques to time-series data, capable of extracting
+    and manipulating frequency components. It supports both simple Fourier Transform and attention-based
+    frequency analysis, allowing for the extrapolation and prediction based on frequency patterns.
 
+    Parameters
+    ----------
+    pred_len : int
+        The length of the sequence to predict using the frequency components.
+    k : int, optional
+        The number of top frequency components to retain for the analysis. Default is None.
+    low_freq : int, optional
+        The lowest frequency index to consider in the analysis to avoid zero-frequency (DC) components.
+        Default is 1.
+    output_attention : bool, optional
+        Flag to determine if the layer should output the attention weights. Default is False.
+
+    """
     def __init__(self, pred_len, k=None, low_freq=1, output_attention=False):
         super().__init__()
         # self.d_model = d_model
@@ -215,7 +393,22 @@ class FourierLayer(nn.Module):
         self.output_attention = output_attention
 
     def forward(self, x):
-        """x: (b, t, d)"""
+        """
+        Executes the Fourier transform operations on the input data 'x'. Depending on the configuration, it performs
+        a basic Fourier transform to analyze frequency components, selects top-k frequencies, and optionally performs
+        an inverse transform to either reconstruct or predict time-series data.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input time-series data tensor with shape (batch_size, time_steps, features).
+
+        Returns
+        -------
+        torch.Tensor, tuple(torch.Tensor, torch.Tensor)
+            Depending on the `output_attention` setting, it returns either the transformed data tensor or
+            a tuple containing the transformed data tensor and attention weights.
+        """
 
         if self.output_attention:
             return self.dft_forward(x)
@@ -259,6 +452,26 @@ class FourierLayer(nn.Module):
         return x_freq, index_tuple
 
     def dft_forward(self, x):
+        """
+        Applies Discrete Fourier Transform (DFT) and its inverse (iDFT) to extract and use the most
+        significant frequency components for extrapolation or reconstruction of signals. This method
+        specifically computes a frequency-domain representation using the DFT, selects the most
+        significant frequencies, and applies an inverse DFT to produce a predicted output.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input time-series data tensor with shape (batch_size, time_steps, features).
+
+        Returns
+        -------
+        tuple(torch.Tensor, torch.Tensor)
+            A tuple containing:
+            - The extrapolated or reconstructed time-series data based on the significant frequency components.
+            - The attention weights across the frequency domain shaped for visualization or further analysis,
+            indicating the importance of each frequency component.
+        
+        """
         T = x.size(1)
 
         dft_mat = fft.fft(torch.eye(T))
@@ -298,7 +511,15 @@ class FourierLayer(nn.Module):
     
 class series_decomp(nn.Module):
     """
-    Series decomposition block
+    A PyTorch module for series decomposition using a single moving average kernel. This module decomposes
+    a time series into its residual and moving average components, providing a straightforward approach to
+    trend extraction.
+
+    Parameters
+    ----------
+    kernel_size : int
+        The kernel size to be used for the moving average calculation. The kernel size defines the window
+        for the moving average.
     """
 
     def __init__(self, kernel_size):
@@ -306,6 +527,22 @@ class series_decomp(nn.Module):
         self.moving_avg = moving_avg(kernel_size, stride=1)
 
     def forward(self, x):
+        """
+        Applies the series decomposition to the input tensor 'x'. It calculates the moving average using the specified kernel size
+        and computes the residual by subtracting the moving average from the original series.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input time-series data tensor, typically of shape (batch_size, time_steps).
+
+        Returns
+        -------
+        tuple(torch.Tensor, torch.Tensor)
+            A tuple containing:
+            - The residual tensor after subtracting the moving average from the input.
+            - The moving average tensor computed from the input.
+        """
         moving_mean = self.moving_avg(x)
         res = x - moving_mean
         return res, moving_mean
